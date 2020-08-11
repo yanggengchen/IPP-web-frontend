a2a9c02263da754eec0d6d8984855950194d1985
kevin_wang@sjtu.edu.cn <kevin_wang@sjtu.edu.cn>
2020-08-11 08:43:10 +0800

    一个使用OSM的简单地图测试

---
+++ src/components/OLTest.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="oltest">
+    <div id="map">
+
+    </div>
+  </div>
+</template>
+
+<script>
+    import "ol/ol.css";
+    import {Map, View} from "ol";
+    import TileLayer from "ol/layer/Tile";
+    import OSM from 'ol/source/OSM';
+
+    export default {
+        name: "OLTest",
+        data() {
+          return {
+            map: null
+          };
+        },
+        mounted() {
+          this.map = new Map({
+            target: "map",
+            layers: [
+              new TileLayer({
+                source: new OSM()
+              })
+            ],
+            view: new View({
+              center: [0, 0],
+              zoom: 0,
+            })
+          });
+        }
+    }
+</script>
+
+<style scoped>
+  #oltest {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+
+  #map {
+    width: 100%;
+    height: 100%;
+  }
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import OLTest from '@/components/OLTest'
 
 Vue.use(Router);
 
@@ -10,6 +11,12 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: HelloWorld,
+      meta: {title: "无人机"}
+    },
+    {
+      path: '/oltest',
+      name: 'OLTest',
+      component: OLTest,
       meta: {title: "无人机"}
     }
   ]
